44e3789df606a5fffb3d0e89fc67f27f177e61c1
kodman <ufuk.kodman@gmail.com>
2023-01-04 20:17:51 +0100

    add stryker test

---
+++ src/sumArray.js
@@ -0,0 +1,8 @@
+const sumArrayItems = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+module.exports = sumArrayItems;
---
+++ src/sumArray.test.js
@@ -0,0 +1,11 @@
+import sumArrayItems from './sumArray';
+
+it('should return 0 if there is no items in array', () => {
+  const result = sumArrayItems([]);
+  expect(result).toBe(0);
+});
+
+it('should return value if there is  items in array', () => {
+  const result = sumArrayItems([1, 3, 5]);
+  expect(result).toBe(9);
+});
