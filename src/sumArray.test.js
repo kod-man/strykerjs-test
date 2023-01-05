91f3f6f0e16e62c091d6a91ff18c7d2ed23759fd
kodman <ufuk.kodman@gmail.com>
2023-01-05 09:51:41 +0100

    run stryker

--- src/sumArray.test.js
+++ src/sumArray.test.js
@@ -5,7 +5,7 @@ it('should return 0 if there is no items in array', () => {
   expect(result).toBe(0);
 });
 
-it('should return value if there is  items in array', () => {
-  const result = sumArrayItems([1, 3, 5]);
-  expect(result).toBe(9);
-});
+// it('should return value if there is  items in array', () => {
+//   const result = sumArrayItems([1, 3, 5]);
+//   expect(result).toBe(9);
+// });
